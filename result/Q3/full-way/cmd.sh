efea0bfae8e93e813464eb063a3f9be039176ad0
qmzp93 <b31901937@gmail.com>
2025-06-11 17:25:03 +0800

    Q3

---
+++ result/Q3/full-way/cmd.sh
@@ -0,0 +1,1 @@
+./build/X86/gem5.opt configs/example/se.py -c ../benchmark/quicksort --cpu-type=TimingSimpleCPU --caches --l2cache --l3cache --l3_assoc=1 --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=256kB --mem-type=NVMainMemory --nvmain-config=../NVmain/Config/PCM_ISSCC_2012_4GB.config
